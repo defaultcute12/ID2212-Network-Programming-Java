5d4cbd3c79d3b45bba42a2d4715c322df6a843f5
Paul Coada <coada@kth.se>
2016-11-09 16:12:00 +0100

    finish Hangman

--- HW1/server/Hangman.java
+++ HW1/server/Hangman.java
@@ -1,12 +1,21 @@
+package se.coada.id2212.hw1;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 
 public class Hangman {
 	
+	private final String WORDFILE = "words.txt";
 	private final int GUESSES = 6;
 	private final int WONGAME = -1;
 	
 	private int remainingGuesses;
 	private int score;
-	private String obfuscatedWord;
+	private char[] obfuscatedWord;
 	private String cleanWord;
 	
 	public Hangman()
@@ -14,19 +23,45 @@ public class Hangman {
 		score = 0;
 	}
 	
+	/*
+	 * Handles all of the file openings and readings
+	 */
+	private String getWord()
+	{
+		String word = "Programming";
+		try {
+			// Open words.txt
+			BufferedReader br = new BufferedReader(new FileReader(WORDFILE));
+			String tempWord;
+			
+			for (int i = 0; i < 10; i++)									// Read random number of lines
+			{
+				if ( (tempWord = br.readLine()) == null) break;				// EOF
+				word = tempWord;										// Assign new word to cleanWord
+			}
+			br.close();
+		}
+		catch (FileNotFoundException e) {
+			System.err.println("Hangman failed opening " + WORDFILE + ", will set word to 'programming'");
+		}
+		catch (IOException e) {
+			System.err.println("Hangman opened " + WORDFILE + " but something happened");
+		}
+		
+		return word;
+	}
+	
 	public GameState newGame()
 	{
-		remainingGuesses = GUESSES;
+		remainingGuesses = GUESSES;						// reset the number of guesses
 		
-		// TODO: Open words.txt
-		// TODO: Read random number of lines
-		// TODO: Assign new word to cleanWord
-		
-		// We only work with lower case
-		cleanWord = cleanWord.toLowerCase();
+		cleanWord = getWord();							// generate a new word
+						
+		cleanWord = cleanWord.toLowerCase();			// make sure word is lowercase
 		
 		// obfuscate word returned to player
-		obfuscatedWord = StringUtils.repeat("-", cleanWord.length);
+		obfuscatedWord = new char[cleanWord.length()];
+		Arrays.fill(obfuscatedWord, '-');
 		
 		return new GameState();
 	}
@@ -36,15 +71,23 @@ public class Hangman {
 		if (remainingGuesses > 0)
 		{
 			int firstIndex;
-			
 			c = Character.toLowerCase(c);
-			
-			// if word contains c and it has never been guessed before
-			if ( (firstIndex = cleanWord.indexOf(c)) != -1 && obfuscatedWord.indexOf(c) == -1)
+						
+			if ( (firstIndex = cleanWord.indexOf(c)) != -1)					// if word contains char
 			{
-				// TODO: start at firstIndex and reveal all c in obfuscatedWord
-				
-				if (obfuscatedWord.equals(cleanWord)) victory();
+				for (int i = firstIndex; i < cleanWord.length(); i++)
+				{
+					if (cleanWord.charAt(i) == c)							// this char in word is char
+					{
+						if (obfuscatedWord[i] == c)							// this char in obf. has already been revealed
+						{
+							badGuess();										// consider this char guess as bad
+							return new GameState();
+						}
+						obfuscatedWord[i] = c;								// reveal the char
+					}
+				}
+				if (new String(obfuscatedWord).equals(cleanWord)) victory();
 			}
 			else badGuess();
 		}
@@ -65,14 +108,14 @@ public class Hangman {
 	private void badGuess()
 	{
 		remainingGuesses--;
-		if (remainingGuesses == 0) obfuscatedWord = "Game Over! Correct word was " + cleanWord;
+		if (remainingGuesses == 0) obfuscatedWord = ("Game Over! Correct word was " + cleanWord).toCharArray();
 	}
 	
 	private void victory()
 	{
 		score++;
-		obfuscatedWord = "Congratulations! You guessed " + cleanWord;
-		remaningGuesses = WONGAME;
+		obfuscatedWord = ("Congratulations! You guessed " + cleanWord).toCharArray();
+		remainingGuesses = WONGAME;
 	}
 	
 	// The game state object, to be returned to the player
@@ -85,7 +128,7 @@ public class Hangman {
 		public GameState()
 		{
 			this.remainingGuesses = Hangman.this.remainingGuesses;
-			this.obfuscatedWord = Hangman.this.obfuscatedWord;
+			this.obfuscatedWord = new String(Hangman.this.obfuscatedWord);
 			this.score = Hangman.this.score;
 		}
 	}
